a2ca3d3e1f866cfe4ce8100f7cc18b46bce4e703
walk_to_work <fallere15423@gmail.com>
2019-10-19 12:04:19 +0900

    テスト用json追加

---
+++ street_view_api_test/main.js
@@ -0,0 +1,38 @@
+var KEY = 'AIzaSyDI8c1xJ4xr9VGlhAmSye4-2fWqQ2Dh_gc'
+
+var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json'
+var request = new XMLHttpRequest()
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send()
+
+request.onload = function() {
+  var res = request.response;
+	console.log(res)
+}
+
+$(function(){
+	$('#btn').on('click',function(){
+		let latitude = '34'
+		let longitude = '135.988354'
+		let loc = latitude + ',' + longitude
+		let heading = '0'
+
+		console.log('緯度:' + latitude + ' 経度:' + longitude +  ' 方位:' + heading );
+
+		var url = 'https://maps.googleapis.com/maps/api/streetview?size=400x400&location=' + loc + '&fov=120&heading=' +  heading + '&pitch=10&key=' + KEY
+		var img=new Image();
+		img.onload=function(e){
+			var width= img.width;
+			var height= img.height;
+			var cnvsH = 400;
+			var cnvsW = parseInt(width * cnvsH / height);
+			var canvas = document.querySelector('#canvas');
+			canvas.setAttribute('width', cnvsW);
+			canvas.setAttribute('height', cnvsH);
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(img, 0, 0, cnvsW, cnvsH);
+		}
+		img.src=url;
+	});
+});
