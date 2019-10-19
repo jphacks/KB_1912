4b473a0c584399d67536f8188c150f7d060acc0a
walk_to_work <fallere15423@gmail.com>
2019-10-19 15:11:05 +0900

    とりあえず終わり

--- street_view_api_test/main.js
+++ street_view_api_test/main.js
@@ -10,9 +10,20 @@ $.getJSON("test2.json", function(json) {
 $(function(){
 	$('#btn').on('click',function(){
 
+
+		/*
+		//インスタンスの生成
+		gifAnimation = new GIFEncoder();
+		//コマ毎の遅延時間（ミリ秒）
+		gifAnimation.setDelay(0);
+		//GIFアニメーションの生成開始
+		gifAnimation.start();
+		*/
+
 		let bef_lat = null , bef_lng = null;
 
 		steps.forEach(function(element) {
+
 			let lat = element.start_location.lat
 			let lng = element.start_location.lng
 
@@ -33,6 +44,8 @@ $(function(){
 				var ctx = canvas.getContext('2d');
 				ctx.drawImage(img, 0, 0, cnvsW, cnvsH);
 				document.body.appendChild(canvas);
+
+				//gifAnimation.addFrame( canvas );
 			}
 			img.src=url;
 
@@ -40,6 +53,28 @@ $(function(){
 			bef_lng = lng
 
 		});
+
+		/*
+		gifAnimation.finish();
+		//バイナリデータの編集
+		var byteString = gifAnimation.stream().getData() ;
+		var ab = new ArrayBuffer(byteString.length);
+		var ia = new Uint8Array(ab);
+		for (var i = 0; i < byteString.length; i++) {
+			ia[i] = byteString.charCodeAt(i);
+		}
+		// Blobオブジェクトの生成
+		var blob = new Blob( [ab], {type: "image/gif" });
+		//a要素の生成
+		var a = document.createElement("a");
+		//BlobURLを取得しa要素のsrcへ与える
+		a.href = window.URL.createObjectURL( blob );
+		//PNGファイル名の命名
+		a.download = "animation.gif";
+		a.innerHTML = "gifアニメーション";
+		//body要素にa要素を追加
+		document.getElementsByTagName( "body" )[0].appendChild(a);
+		*/
 
 	});
 });
